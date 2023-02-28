d335c69d82d92df138d046a52bfac1fdf267e269
panchotoni <panchotonidandel97@gmail.com>
2023-02-28 19:26:36 -0300

    Tarea 2

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import operaciones as op
-suma1 = op.suma(18, 12)
-suma2 = op.suma(15, 16)
+import operaciones as t
+hora = int(input("Digite la hora en la que se encuentra: "))
 
-print(suma1)
-print(suma2)
-print(op.resta(5, 4))
-print(op.multi(5, 5))
-print(op.divi(5, 5))
+print(t.horis(hora))
--- operaciones.py
+++ operaciones.py
@@ -9,3 +9,13 @@ def multi(a, b):
 
 def divi(a, b):
     return a / b
+def horis(a):
+    if a >= 19 and a <= 24:
+        return "Es hora de irse del job"
+    elif a < 19 and a >= 0:
+        falta = a - 19
+        abs(falta)
+        return "Es hora de jobear, te quedan: ", abs(falta)
+    else:
+        return "El dia tiene 24 hs estupido"
+
